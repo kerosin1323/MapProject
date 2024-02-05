dadae49f379769bc2f5f9ede2c45497d7db81f4d
kerosin1323 <ermolenkokirill132@icloud.com>
2024-02-05 20:00:37 +0300

    Первое задание

---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+import sys
+
+import pygame
+import requests
+
+coord = input('Введите координаты: ')
+spn = input('Введите масштаб: ')
+
+map_request = f"http://static-maps.yandex.ru/1.x/?ll={''.join(coord)}&spn={spn}&l=map"
+response = requests.get(map_request)
+
+if not response:
+    print("Ошибка выполнения запроса:")
+    print(map_request)
+    print("Http статус:", response.status_code, "(", response.reason, ")")
+    sys.exit(1)
+
+# Запишем полученное изображение в файл.
+map_file = "map.png"
+with open(map_file, "wb") as file:
+    file.write(response.content)
+
+# Инициализируем pygame
+pygame.init()
+screen = pygame.display.set_mode((600, 450))
+# Рисуем картинку, загружаемую из только что созданного файла.
+screen.blit(pygame.image.load(map_file), (0, 0))
+# Переключаем экран и ждем закрытия окна.
+pygame.display.flip()
+while pygame.event.wait().type != pygame.QUIT:
+    pass
+pygame.quit()
+
+# Удаляем за собой файл с изображением.
+os.remove(map_file)
