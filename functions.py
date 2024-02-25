05be15b49ab7a6d5ef35a9f555a8986acdbfc937
kerosin1323 <ermolenkokirill132@icloud.com>
2024-02-25 20:08:27 +0300

    Задание 5

---
+++ functions.py
@@ -0,0 +1,57 @@
+import pygame
+import sys
+import requests
+
+
+class TextInputBox(pygame.sprite.Sprite):
+    """Создание строки-ввода"""
+    def __init__(self, x, y, w, font, function):
+        super().__init__()
+        self.color = (255, 255, 255)
+        self.backcolor = None
+        self.function = function
+        self.pos = (x, y)
+        self.width = w
+        self.font = font
+        self.active = False
+        self.text = ""
+        self.render_text()
+
+    def render_text(self):
+        t_surf = self.font.render(self.text, True, self.color, self.backcolor)
+        self.image = pygame.Surface((max(self.width, t_surf.get_width()+10), t_surf.get_height()+10), pygame.SRCALPHA)
+        if self.backcolor:
+            self.image.fill(self.backcolor)
+        self.image.blit(t_surf, (5, 5))
+        pygame.draw.rect(self.image, self.color, self.image.get_rect().inflate(-2, -2), 2)
+        self.rect = self.image.get_rect(topleft=self.pos)
+
+    def update(self, event_list):
+        for event in event_list:
+            if event.type == pygame.MOUSEBUTTONDOWN and not self.active:
+                self.active = self.rect.collidepoint(event.pos)
+            if event.type == pygame.KEYDOWN and self.active:
+                if event.key == pygame.K_RETURN:
+                    self.function()
+                elif event.key == pygame.K_BACKSPACE:
+                    self.text = self.text[:-1]
+                else:
+                    self.text += event.unicode
+            self.render_text()
+
+
+def get_response(map_request):
+    response = requests.get(map_request)
+
+    if not response:
+        print("Ошибка выполнения запроса:")
+        print(map_request)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        sys.exit(1)
+
+    # Запишем полученное изображение в файл.
+    map_file = "map.png"
+    with open(map_file, "wb") as file:
+        file.write(response.content)
+
+    return map_file, bool(response)
--- main.py
+++ main.py
@@ -1,46 +1,65 @@
 import os
-import sys
-
+from functions import *
 import pygame
-import requests
 
 spn_to_px_convert = {1: 0, 2: 0, 3: 0.5, 4: 0.5, 5: 0.1, 6: 0.1, 7: 0.08, 8: 0.08, 9: 0.06, 10: 0.04, 11: 0.04}
-coord = input('Введите координаты: ')
-spn = input('Введите масштаб: ')
-coord_list = coord.split(',')
-coord_list[0] = float(coord_list[0])
-coord_list[1] = float(coord_list[1])
-coord = str(coord_list[1]) + "," + str(coord_list[0])
-if int(spn) < 6:
-    zoom = 0.001
-elif 5 < int(spn) < 12:
-    zoom = 0.007
-else:
-    zoom = 0.01
-
-map_request = f"http://static-maps.yandex.ru/1.x/?ll={''.join(coord)}&z={spn}&l=map"
-response = requests.get(map_request)
-
-if not response:
-    print("Ошибка выполнения запроса:")
-    print(map_request)
-    print("Http статус:", response.status_code, "(", response.reason, ")")
-    sys.exit(1)
-
-# Запишем полученное изображение в файл.
-map_file = "map.png"
-with open(map_file, "wb") as file:
-    file.write(response.content)
-
+spn = 10
+zoom = 0.007
+l = 'map'
 # Инициализируем pygame
 pygame.init()
 screen = pygame.display.set_mode((600, 450))
 # Рисуем картинку, загружаемую из только что созданного файла.
-screen.blit(pygame.image.load(map_file), (0, 0))
+screen.fill((0, 0, 0))
 # Переключаем экран и ждем закрытия окна.
 pygame.display.flip()
-work = True
-while work:  # цикл работы карты
+work = False
+text = ''
+
+
+class Search:
+    def __init__(self, screen):
+        self.screen = screen
+
+    def update(self):
+        self.screen.fill((0, 0, 0))
+        font = pygame.font.Font(None, 50)
+        self.text_input_box = TextInputBox(50, 200, 500, font, self.func)
+        group = pygame.sprite.Group(self.text_input_box)
+        self.running = True
+        while self.running:
+            self.screen.fill((0, 0, 0))
+            event_list = pygame.event.get()
+            for event in event_list:
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+            group.update(event_list)
+            # заголовок
+            self.textSurface = font.render('Введите объект: ', True, (255, 255, 255), None)
+            self.textRect = self.textSurface.get_rect(center=(300, 175))
+            self.screen.blit(self.textSurface, self.textRect)
+            group.draw(screen)
+            pygame.display.flip()
+
+    def func(self):
+        global work, text
+        work = True
+        text = self.text_input_box.text
+        self.running = False
+
+
+searching = Search(screen)
+searching.update()
+geocoder_request = f"http://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&geocode={text}&format=json"
+geocoder_response = requests.get(geocoder_request).json()["response"]["GeoObjectCollection"]["featureMember"][0][
+    "GeoObject"]
+coord = geocoder_response["Point"]["pos"].split()
+coord_list = list(map(float, geocoder_response["Point"]["pos"].split()))
+map_request = f"http://static-maps.yandex.ru/1.x/?ll={','.join(coord)}&z={spn}&l=map"
+map_file = get_response(map_request)[0]
+
+while work:
+    screen.blit(pygame.image.load(map_file), (0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:  # выход с проги
             pygame.quit()
@@ -74,7 +93,6 @@ while work:  # цикл работы карты
                     else:
                         zoom = 0.001
                         print(zoom)
-                    map_request = f"http://static-maps.yandex.ru/1.x/?ll={''.join(coord)}&z={spn}&l=map"
 
             if event.key == pygame.K_PAGEDOWN:  # уменьшение
                 if int(spn) - 1 > 0:  # границы уменьшения
@@ -104,55 +122,36 @@ while work:  # цикл работы карты
                     else:
                         zoom = 0.001
                         print(zoom)
-                    map_request = f"http://static-maps.yandex.ru/1.x/?ll={''.join(coord)}&z={spn}&l=map"
 
             if event.key == pygame.K_DOWN:  # движение вниз
-                if coord_list[0] - zoom > -90:  # границы движения
-                    coord_list[0] -= zoom
-                    coord = str(coord_list[1]) + "," + str(coord_list[0])
-                map_request = f"http://static-maps.yandex.ru/1.x/?ll={''.join(coord)}&z={spn}&l=map"
+                if coord_list[1] - zoom > -90:  # границы движения
+                    coord_list[1] -= zoom
 
             if event.key == pygame.K_UP:  # движение вверх
-                if coord_list[0] + zoom < 90:  # границы движения
-                    coord_list[0] += zoom
-                    coord = str(coord_list[1]) + "," + str(coord_list[0])
-                map_request = f"http://static-maps.yandex.ru/1.x/?ll={''.join(coord)}&z={spn}&l=map"
+                if coord_list[1] + zoom < 90:  # границы движения
+                    coord_list[1] += zoom
 
             if event.key == pygame.K_RIGHT:  # движение вправо
-                if coord_list[1] + zoom < 180:  # границы движения
-                    coord_list[1] += zoom
-                    coord = str(coord_list[1]) + "," + str(coord_list[0])
-                map_request = f"http://static-maps.yandex.ru/1.x/?ll={''.join(coord)}&z={spn}&l=map"
+                if coord_list[0] + zoom < 180:  # границы движения
+                    coord_list[0] += zoom
 
             if event.key == pygame.K_LEFT:  # движение влево
-                if coord_list[1] - zoom > -180:  # границы движения
-                    coord_list[1] -= zoom
-                    coord = str(coord_list[1]) + "," + str(coord_list[0])
-                map_request = f"http://static-maps.yandex.ru/1.x/?ll={''.join(coord)}&z={spn}&l=map"
+                if coord_list[0] - zoom > -180:  # границы движения
+                    coord_list[0] -= zoom
 
             if event.key == pygame.K_1:
-                map_request = f"http://static-maps.yandex.ru/1.x/?ll={''.join(coord)}&z={spn}&l=sat"
+                l = "sat"
 
             if event.key == pygame.K_2:
-                map_request = f"http://static-maps.yandex.ru/1.x/?ll={''.join(coord)}&z={spn}&l=sat,skl"
+                l = 'sat,skl'
 
             if event.key == pygame.K_3:
-                map_request = f"http://static-maps.yandex.ru/1.x/?ll={''.join(coord)}&z={spn}&l=map"
+                l = 'map'
 
-        response = requests.get(map_request)  # создание нового запроса
-        if not response:  # проверка на ошибки
-            pygame.quit()
-            work = False
-            print("Ошибка выполнения запроса:")
-            print(map_request)
-            print("Http статус:", response.status_code, "(", response.reason, ")")
-            sys.exit(1)
-        map_file = "map.png"
-        with open(map_file, "wb") as file:
-            file.write(response.content)
+        coord = [str(coord_list[0]), str(coord_list[1])]
+        print(coord)
+        map_request = f"http://static-maps.yandex.ru/1.x/?ll={','.join(coord)}&z={spn}&l={l}"
+        map_file, work = get_response(map_request)
         screen.blit(pygame.image.load(map_file), (0, 0))  # вывод нового фрагмента карты на экран
         pygame.display.flip()
-        os.remove(map_file)  # удаление фото после вывода, для экономии памяти
 
-# Удаляем за собой файл с изображением.
-os.remove(map_file)
